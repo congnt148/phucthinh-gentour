29465bb559ad46710d7089ce2abcd4e8082479a2
cong.nguyenthanh <cong.nguyenthanh@teragroup.io>
2023-12-08 00:29:01 +0700

    c

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import fs from "fs";
+import path from "path";
 
 function App() {
+  const exportHTML = () => {
+    const contentPath = path.join(process.cwd(), "./templater.html");
+    const content = fs.readFileSync(contentPath, "utf-8");
+
+    var source =
+      "data:application/vnd.ms-word;charset=utf-8," +
+      encodeURIComponent(content);
+    var fileDownload = document.createElement("a");
+    document.body.appendChild(fileDownload);
+    fileDownload.href = source;
+    fileDownload.download = "document.doc";
+    fileDownload.click();
+    document.body.removeChild(fileDownload);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +26,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => exportHTML()}>Gen</button>
       </header>
     </div>
   );
